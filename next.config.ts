89356bfc97fbdc7541acebc5a94621daf7337d95
Christopher Aedo <christopher.aedo.p@gmail.com>
2025-05-05 11:48:33 -0400

    fin seccion 4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+  //para imagenes remotas
+	images: {
+		remotePatterns: [new URL('https://images.unsplash.com/*')],
+	},
 };
 
 export default nextConfig;
---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,61 @@
+/** @format */
+
+import Image from 'next/image';
+import { FiHexagon } from 'react-icons/fi';
+import { SidebarMenuItems } from './SidebarMenuItems';
+import { IoBrowsersOutline, IoCalculator } from 'react-icons/io5';
+
+const menuItems = [
+	{
+		path: '/dashboard/main',
+		icon: <IoBrowsersOutline size={35} />,
+		title: 'Dashboard',
+		subTitle: 'Visualizacion',
+	},
+	{
+		path: '/dashboard/counter',
+		icon: <IoCalculator size={35} />,
+		title: 'Counter',
+		subTitle: 'Contador Client Side',
+	},
+];
+
+export default function Sidebar() {
+	return (
+		<div
+			style={{ width: '350px' }}
+			id="menu"
+			className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+			<div id="logo" className="my-4 px-6">
+				<h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
+					<FiHexagon className="mr-2" />
+					<span>Dash</span>
+					<span className="text-blue-500">8</span>.
+				</h1>
+				<p className="text-slate-500 text-sm">
+					Manage your actions and activities
+				</p>
+			</div>
+			<div id="profile" className="px-6 py-10">
+				<p className="text-slate-500">Welcome back,</p>
+				<a href="#" className="inline-flex space-x-2 items-center">
+					<span>
+						<Image
+							className="rounded-full w-8 h-8"
+							src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
+							alt=""
+							width={50}
+							height={50}
+						/>
+					</span>
+					<span className="text-sm md:text-base font-bold">Edward Tompson</span>
+				</a>
+			</div>
+			<div id="nav" className="w-full px-6">
+				{menuItems.map((item) => (
+					<SidebarMenuItems key={item.path} {...item} />
+				))}
+			</div>
+		</div>
+	);
+}
